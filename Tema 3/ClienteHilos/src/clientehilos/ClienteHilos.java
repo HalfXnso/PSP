8cf53a46ec2f33addc7ed1e518c65eac30d27138
HalfXnso <alfonsogaitanperez@gmail.com>
2024-11-05 13:49:48 +0100

    ASDASD

---
+++ Tema 3/ClienteHilos/src/clientehilos/ClienteHilos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientehilos;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author AlfonsoGaitanPerez
+ */
+public class ClienteHilos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Scanner tec = new Scanner(System.in);
+
+        Socket cliente = new Socket("localhost", 6000);
+
+        DataInputStream entrada = new DataInputStream(cliente.getInputStream());
+
+        DataOutputStream salida = new DataOutputStream(cliente.getOutputStream());
+
+        String cadena = "";
+        while (!cadena.trim().equals("*")) {
+            System.out.println("Introducir Texto: ");
+            cadena = tec.nextLine();
+            salida.writeUTF(cadena);
+            String mayus = entrada.readUTF();
+            System.out.println("Texto Convertido: " + mayus);
+
+        }
+        entrada.close();
+        salida.close();
+        cliente.close();
+        System.out.println("Conexion finalizada");
+
+    }
+
+}
---
+++ Tema 3/ServidorHilos/src/servidorhilos/ServidorHilos.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorhilos;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author AlfonsoGaitanPerez
+ */
+public class ServidorHilos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+        ServerSocket servidor = new ServerSocket(6000);
+        System.out.println("Servidor iniciado....");
+        while(true){
+            Socket servidorCliente = new Socket();
+            servidorCliente = servidor.accept();
+            
+            Servidor hiloServidor = new Servidor(servidorCliente);
+            hiloServidor.start();
+            
+        }
+        
+    }
+
+}
+
+class Servidor extends Thread {
+
+    DataInputStream entrada;
+    DataOutputStream salida;
+    Socket socket = null;
+
+    public Servidor(Socket s) throws IOException {
+
+        this.socket = s;
+        this.entrada = new DataInputStream(socket.getInputStream());
+        this.salida = new DataOutputStream(socket.getOutputStream());
+
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            String cadena = "";
+            System.out.println("Conexion: " + this.socket.toString());
+
+            while (!cadena.trim().equals("*")) {
+                cadena = entrada.readUTF();
+                salida.writeUTF(cadena.trim().toUpperCase());
+            }
+            System.out.println("Fin Conexión: " + socket.toString());
+
+            entrada.close();
+            salida.close();
+            socket.close();
+        } catch (IOException ex) {
+        }
+
+    }
+
+}
